e18189c92531d376f474d9333745f0bc0d56e11c
FlackAP <flack.ap@gmail.com>
2014-10-19 15:05:03 -0400

    Deployed: i have sinned and used bangImportant. whoops. header fixes
    generated from commit cdbbdd7408f764b9f4534021982fbd051a22c3d8

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,14 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+    $(window).scroll(function() {
+        if ($('.navbar').offset().top > 50) {
+            $('.navbar-fixed-top').addClass('top-nav-collapse');
+        } else {
+            $('.navbar-fixed-top').removeClass('top-nav-collapse');
+        }
+    });
 });
 
 // Closes the Responsive Menu on Menu Item Click
