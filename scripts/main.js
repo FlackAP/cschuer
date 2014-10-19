81b6f1019600c3664cf2be1f6da1c4cd2bdd9e77
FlackAP <flack.ap@gmail.com>
2014-10-19 15:07:06 -0400

    Deployed: i have sinned and used bangImportant. whoops. header fixes
    generated from commit cdbbdd7408f764b9f4534021982fbd051a22c3d8

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
-'use strict';
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($('.navbar').offset().top > 50) {
-        $('.navbar-fixed-top').addClass('top-nav-collapse');
-    } else {
-        $('.navbar-fixed-top').removeClass('top-nav-collapse');
-    }
-});
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -16,14 +7,6 @@ $(function() {
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
-    });
-
-    $(window).scroll(function() {
-        if ($('.navbar').offset().top > 50) {
-            $('.navbar-fixed-top').addClass('top-nav-collapse');
-        } else {
-            $('.navbar-fixed-top').removeClass('top-nav-collapse');
-        }
     });
 });
 
